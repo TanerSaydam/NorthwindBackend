22dbf627f37f869d2b1d4ee8777d7601d1a73cfc
Taner Saydam <83103843+TanerSaydam@users.noreply.github.com>
2021-12-05 21:33:06 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFreamwork/Context/NorthwindContext.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFreamwork.Context
+{
+    public class NorthwindContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-3BJ5GK9;Database=Northwind;Trusted_Connection=true");
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
